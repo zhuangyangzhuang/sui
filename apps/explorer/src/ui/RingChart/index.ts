1aca78d0b34cd5dcde510c7f76690a9633544e28
Phong Lam <phonglam@Phongs-Laptop.attlocal.net>
2023-04-20 13:49:43 -0700

    refactor ring chart file

---
+++ apps/explorer/src/ui/RingChart/index.ts
@@ -0,0 +1,5 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+export { RingChart as default, type RingChartProps } from './RingChart';
+export { RingChartLegend } from './RingChartLegend';
